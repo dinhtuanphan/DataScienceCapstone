23c4ea24970f602afd6f4fbb3b6a1e318632eb30
Tuan Phan <56570502+dinhtuanphan@users.noreply.github.com>
2021-05-01 15:19:03 -0400

    Delete week1 quiz.R

--- week1 quiz.R
+++
@@ -1,9 +0,0 @@
-size <- file.info('./final/en_US/en_US.blogs.txt')
-kb <- size$size/1024
-mb<- kb/1024
-mb
-
-twitter <- readLines(con = file('./final/en_US/en_US.twitter.txt'),
-                     encoding = "UTF-8", skipNul = TRUE)
-length(twitter)
-
